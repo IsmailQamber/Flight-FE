3a17431421f7f5b30f30aa4393b158b226071a9b
AymanAlshaikh <ayman159@live.com>
2021-03-10 20:58:36 +0300

    3dlna elcod mal elmain

--- src/components/FlightItems.js
+++ src/components/FlightItems.js
@@ -1,22 +1,14 @@
 import {
   IconButton,
-  List,
-  ListItem,
   ListItemText,
-  Table,
-  TableBody,
   TableCell,
-  TableHead,
   TableRow,
 } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
-import ImageIcon from "@material-ui/icons/Image";
-import WorkIcon from "@material-ui/icons/Work";
-import BeachAccessIcon from "@material-ui/icons/BeachAccess";
-import { AirplanemodeActiveTwoTone, Book, Send } from "@material-ui/icons";
+import { AirplanemodeActiveTwoTone, Book } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,71 +18,60 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const FlightItems = ({ flight }) => {
+const FlightItems = ({ flight, setId }) => {
   const classes = useStyles();
   return (
-    <div>
-      <Table size="small">
-        {/* <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell>Airline</TableCell>
-            <TableCell>Price</TableCell>
-            <TableCell>Airport</TableCell>
-            <TableCell>Time</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Seats</TableCell>
-            <TableCell>Book</TableCell>
-          </TableRow>
-        </TableHead> */}
-        <TableBody>
-          <TableRow>
-            <TableCell>
-              <ListItemAvatar>
-                <Avatar>
-                  <AirplanemodeActiveTwoTone />
-                </Avatar>
-              </ListItemAvatar>
-            </TableCell>
-            <TableCell>
-              <ListItemText primary={flight.Airline} />
-            </TableCell>
-            <TableCell>
-              <ListItemText primary={`${flight.price} `} secondary="BD" />
-            </TableCell>
-            <TableCell>
-              <ListItemText
-                primary={` ${flight.departureAirport}`}
-                secondary={` ${flight.arrivalAirport}`}
-              />
-            </TableCell>
-            <TableCell>
-              <ListItemText
-                primary={flight.departureTime}
-                secondary={flight.arrivalTime}
-              />
-            </TableCell>
-            <TableCell>
-              <ListItemText
-                primary={flight.departureDate}
-                secondary={flight.arrivalDate}
-              />
-            </TableCell>
-            <TableCell>
-              <ListItemText
-                primary={flight.economySeats}
-                secondary={flight.businessSeats}
-              />
-            </TableCell>
-            <TableCell>
-              <IconButton>
-                <Book />
-              </IconButton>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
+    <TableRow>
+      <TableCell>
+        <ListItemAvatar>
+          <Avatar>
+            <AirplanemodeActiveTwoTone />
+          </Avatar>
+        </ListItemAvatar>
+      </TableCell>
+
+      <TableCell>
+        <ListItemText primary={flight.Airline} />
+      </TableCell>
+
+      <TableCell>
+        <ListItemText primary={`${flight.price} `} secondary="BD" />
+      </TableCell>
+
+      <TableCell>
+        <ListItemText
+          primary={` ${flight.departureAirport}`}
+          secondary={` ${flight.arrivalAirport}`}
+        />
+      </TableCell>
+
+      <TableCell>
+        <ListItemText
+          primary={flight.departureTime}
+          secondary={flight.arrivalTime}
+        />
+      </TableCell>
+
+      <TableCell>
+        <ListItemText
+          primary={flight.departureDate}
+          secondary={flight.arrivalDate}
+        />
+      </TableCell>
+
+      <TableCell>
+        <ListItemText
+          primary={flight.economySeats}
+          secondary={flight.businessSeats}
+        />
+      </TableCell>
+
+      <TableCell>
+        <IconButton onClick={() => setId((array) => [...array, flight.id])}>
+          <Book />
+        </IconButton>
+      </TableCell>
+    </TableRow>
   );
 };
 export default FlightItems;
